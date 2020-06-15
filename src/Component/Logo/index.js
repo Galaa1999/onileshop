dcbd6a7a286a1f80cd707eb6643d5bd2aca0d549
Galaa1999 <53171848+Galaa1999@users.noreply.github.com>
2020-06-15 17:21:23 +0800

    Update index.js

--- src/Component/Logo/index.js
+++ src/Component/Logo/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import css from "./style.module.css";
 import LogoImage from "../../image/logo.png";
-
+//add logo
 const Logo = (props) => (
   <div className={css.Logo}>
     <img alt="" src={LogoImage} />
